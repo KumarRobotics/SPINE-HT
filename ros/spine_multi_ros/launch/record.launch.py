d8fd4daa1418c053440ab95ec207ae84ddc591b4
Zac Ravichandran <zachary.ravichandran@gmail.com>
2025-08-12 18:00:41 -0400

    update record

--- ros/spine_multi_ros/launch/record.launch.py
+++ ros/spine_multi_ros/launch/record.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -20,29 +21,56 @@ def generate_launch_description():
     namespace_arg = LaunchConfiguration("namespace")
     output_arg = LaunchConfiguration("output_bag")
 
-    return LaunchDescription(
-        [
-            declare_namespace_arg,
-            ExecuteProcess(
-                cmd=[
-                    "ros2",
-                    "bag",
-                    "record",
-                    [namespace_arg, "/dlio/odom_node/odom"],
-                    [namespace_arg, "/local_costmap/costmap"],
-                    [namespace_arg, "/global_costmap/costmap"],
-                    [namespace_arg, "/grounding_dino_node/detections"],
-                    [namespace_arg, "/grounding_dino_node/detection_img"],
-                    [namespace_arg, "/cmd_vel"],
-                    [namespace_arg, "/grounding_dino_node/detections_marker"],
-                    [namespace_arg, "/goal_pose"],
-                    [namespace_arg, "/plan"],
-                    "/tf",
-                    "/tf_static",
-                    "-o",
-                    output_arg,
-                ],
+    msg_throttles = [
+        (
+            [namespace_arg, "/zed/depth/depth_registered/compressed"],
+            [namespace_arg, "/zed/throttled/depth/depth_registered/compressed"],
+        ),
+        (
+            [namespace_arg, "/zed/rgb/image_rect_color/compressed"],
+            [namespace_arg, "/zed/throttled/rgb/image_rect_color/compressed"],
+        )(
+            [namespace_arg, "/zed/depth/depth_info"],
+            [namespace_arg, "/zed/throttled/depth/depth_info"],
+        ),
+    ]
+
+    ld = [declare_namespace_arg]
+
+    for orig_topic, throttle_topic in msg_throttles:
+        ld.append(
+            Node(
+                package="topic_tools",
+                executable="throttle",
+                arguments=["messages", orig_topic, "1.0", throttle_topic],
                 output="screen",
-            ),
-        ]
+            )
+        )
+
+    ld.append(
+        ExecuteProcess(
+            cmd=[
+                "ros2",
+                "bag",
+                "record",
+                [namespace_arg, "/dlio/odom_node/odom"],
+                [namespace_arg, "/zed/throttled/depth/depth_registered/compressed"],
+                [namespace_arg, "/zed/throttled/rgb/image_rect_color/compressed"],
+                [namespace_arg, "/local_costmap/costmap"],
+                [namespace_arg, "/global_costmap/costmap"],
+                [namespace_arg, "/grounding_dino_node/detections"],
+                [namespace_arg, "/grounding_dino_node/detection_img"],
+                [namespace_arg, "/cmd_vel"],
+                [namespace_arg, "/grounding_dino_node/detections_marker"],
+                [namespace_arg, "/goal_pose"],
+                [namespace_arg, "/plan"],
+                "/tf",
+                "/tf_static",
+                "-o",
+                output_arg,
+            ],
+            output="screen",
+        )
     )
+
+    return LaunchDescription(ld)
